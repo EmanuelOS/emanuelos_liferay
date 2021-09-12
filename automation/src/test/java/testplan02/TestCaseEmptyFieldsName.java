00e4ee3124b77d8466fe7901baec4f9a8d1d8b5b
EmanuelOS <silva.oliveira.emanuel@gmail.com>
2021-09-12 17:18:25 -0300

    Initial commit for automated test cases. # Automated tests created # ## Test Plan 02 - Basic Form Functionalities ## ### TC01 - Empty fields - All ### TC02 - Empty fields - "What is your name" not empty ### TC03 - Empty fields - "What is your name" and "What is the date of your birth" not empty ### TC04 - Empty fields - "What is your name", "What is the date of your birth" and "Why did you join the testing area" not empty

---
+++ automation/src/test/java/testplan02/TestCaseEmptyFieldsName.java
@@ -0,0 +1,86 @@
+package testplan02;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import resources.Strings;
+
+import static org.junit.Assert.assertEquals;
+import static resources.Strings.requiredFieldEnUs;
+
+
+public class TestCaseEmptyFieldsName {
+
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+
+    @Test
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 2000);
+        new TestCaseEmptyFieldsName().testCaseEmptyFieldsName();
+    }
+
+    @Test
+    public void testCaseEmptyFieldsName() {
+        openForm();
+        stepOne();
+        stepTwo();
+        StepThree();
+        closeWebDrive();
+    }
+
+    private void openForm() {
+        driver.get(Strings.formUrl);
+    }
+
+    @Test
+    private void stepOne() {
+        enterValidName();
+    }
+
+    private void enterValidName() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Strings.xpathNameField)));
+        driver.findElement(By.xpath(Strings.xpathNameField)).sendKeys(Strings.validUserName);
+    }
+
+    private void stepTwo() {
+        submitForm();
+    }
+
+    private void submitForm() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Strings.xpathSubmitForm)));
+        WebElement submitButton = driver.findElement(By.xpath(Strings.xpathSubmitForm));
+        submitButton.submit();
+    }
+
+    private void StepThree() {
+        needRequiredFields();
+    }
+
+    @Test
+    private void needRequiredFields() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Strings.xpathDateBirthFieldRequired)));
+        String dateBirthRequired = driver.findElement(By.xpath(Strings.xpathDateBirthFieldRequired)).getText();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Strings.xpathReasonsFieldRequired)));
+        String reasonsRequired = driver.findElement(By.xpath(Strings.xpathReasonsFieldRequired)).getText();
+        verifyAlerts(dateBirthRequired, reasonsRequired);
+    }
+
+    @Test
+    private void verifyAlerts(String dateBirthRequired, String reasonsRequired) {
+        assertEquals(requiredFieldEnUs, dateBirthRequired);
+        assertEquals(requiredFieldEnUs, reasonsRequired);
+    }
+
+    private void closeWebDrive() {
+        driver.quit();
+    }
+
+}
---
+++ automation/src/test/java/testplan02/TestCaseEmptyFieldsNameBirth.java
@@ -0,0 +1,93 @@
+package testplan02;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import resources.Strings;
+
+import static org.junit.Assert.assertEquals;
+import static resources.Strings.requiredFieldEnUs;
+
+
+public class TestCaseEmptyFieldsNameBirth {
+
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+
+    @Test
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 2000);
+        new TestCaseEmptyFieldsNameBirth().testCaseEmptyFieldsNameBirth();
+    }
+
+    @Test
+    public void testCaseEmptyFieldsNameBirth() throws InterruptedException {
+        openForm();
+        initialSetup();
+        stepOne();
+        stepTwo();
+        stepThree();
+        closeWebDrive();
+    }
+
+    private void initialSetup() {
+        enterValidName();
+    }
+
+    private void enterValidName() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Strings.xpathNameField)));
+        driver.findElement(By.xpath(Strings.xpathNameField)).sendKeys(Strings.validUserName);
+    }
+
+    private void openForm() {
+        driver.get(Strings.formUrl);
+    }
+
+    @Test
+    private void stepOne() throws InterruptedException {
+        enterValidDateOfBirth();
+    }
+
+    private void enterValidDateOfBirth() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Strings.xpathDateBirthField)));
+        driver.findElement(By.xpath(Strings.xpathDateBirthField)).sendKeys(Keys.HOME);
+        driver.findElement(By.xpath(Strings.xpathDateBirthField)).sendKeys(Strings.validDateBirth);
+    }
+
+    private void stepTwo() {
+        submitForm();
+    }
+
+    private void submitForm() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Strings.xpathSubmitForm)));
+        driver.findElement(By.xpath(Strings.xpathSubmitForm)).submit();
+    }
+
+    private void stepThree() {
+        needRequiredFields();
+    }
+
+    @Test
+    private void needRequiredFields() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Strings.xpathReasonsFieldRequired)));
+        String reasonsRequired = driver.findElement(By.xpath(Strings.xpathReasonsFieldRequired)).getText();
+        verifyAlerts(reasonsRequired);
+    }
+
+    @Test
+    private void verifyAlerts(String reasonsRequired) {
+        assertEquals(requiredFieldEnUs, reasonsRequired);
+    }
+
+    private void closeWebDrive() {
+        driver.quit();
+    }
+
+}
